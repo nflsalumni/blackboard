9c93d59f340728e7cf86a344b3a639bd86f8435b
wanghaoalain <wanghaoalain@gmail.com>
2015-01-02 22:22:19 +0100

    Second commit.
    This is the second commit.

--- app/both/router/config.js
+++ app/both/router/config.js
@@ -1,6 +1,7 @@
 Router.configure({
   controller: 'AppController',
-  loadingTemplate: 'loading'
+  loadingTemplate: 'loading',
+  layoutTemplate: 'appLayout'
 });
 
 // Router.plugin('loading', {loadingTemplate: 'loading'});
--- app/both/router/routes.js
+++ app/both/router/routes.js
@@ -5,3 +5,11 @@ Router.route('/', {
 Router.route('/dashboard');
 Router.route('/alumni');
 Router.route('/profile');
+
+Router.route('/photos', {
+  name: 'photos'
+});
+
+Router.route('/activities', {
+  name: 'activities'
+});
