6542c19ff511353f7af3287c63ef00b388cb3a42
Xiyang Chen <settinghead@gmail.com>
2015-01-03 01:23:34 -0500

    Blog integration
    - Hook coding group page with tagged posts
    - Defined tags: ```faq```, ```meeting``` & ```milestone```
    
    meteor-blog documentation:
    https://github.com/Differential/meteor-blog

--- both/controllers/coding-group.js
+++ both/controllers/coding-group.js
@@ -1,7 +1,27 @@
+var POST_LIMIT = 5;
+
 CodingGroupController = AppController.extend({
   waitOn: function() {
+    return [
+      Meteor.subscribe('posts', POST_LIMIT),
+      Meteor.subscribe('taggedPosts', 'meeting'),
+      Meteor.subscribe('taggedPosts', 'milestone'),
+      Meteor.subscribe('taggedPosts', 'faq')
+    ];
   },
   data: {
+    posts: function () {
+      return Post.find({}, {$limit: POST_LIMIT}).fetch();
+    },
+    faqs: function () {
+      return Post.find({tags: 'faq'}, {$limit: POST_LIMIT}).fetch();
+    },
+    meetings: function () {
+      return Post.find({tags: 'meeting'}, {$limit: POST_LIMIT}).fetch();
+    },
+    milestones: function () {
+      return Post.find({tags: 'milestone'}, {$limit: POST_LIMIT}).fetch();
+    }
   },
   onAfterAction: function () {
     Meta.setTitle('建站学习小组专页');
