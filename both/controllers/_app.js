fdcdb854f7ad51fabae0feb118ce1515c3dff392
Xiyang Chen <settinghead@gmail.com>
2015-06-24 23:38:21 -0400

    bring back access control

--- both/controllers/_app.js
+++ both/controllers/_app.js
@@ -1,3 +1,19 @@
 AppController = RouteController.extend({
-  layoutTemplate: 'appLayout'
+  layoutTemplate: 'appLayout',
+  onBeforeAction: function(pause) {
+    if (this.url === '/' ||
+      this.url === '/coding-group/' ||
+      this.url === '/coding-group' ||
+      this.url === '/archive' ||
+      this.url === '/archive/'||
+      this.url === '/signUp' ||
+      this.url === '/signIn') {
+      this.next();
+    } else {
+      if (Meteor.user() === null) {
+        Router.go('signIn');
+      }
+      this.next();
+    }
+  }
 });
